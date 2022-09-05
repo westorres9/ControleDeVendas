db2d4d4e53609a8ae85ed852bc656c3a6f6ac143
westorres9 <wester.torres9@gmail.com>
2022-09-05 18:16:33 -0300

    SaleResource FindById

--- backend/src/main/java/com/devsuperior/ControleDeVendas/resources/SaleResource.java
+++ backend/src/main/java/com/devsuperior/ControleDeVendas/resources/SaleResource.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,11 @@ public class SaleResource {
 		return ResponseEntity.ok().body(page);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<SaleDTO> findById(@PathVariable Long id) {
+		SaleDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
 	
 
 }
