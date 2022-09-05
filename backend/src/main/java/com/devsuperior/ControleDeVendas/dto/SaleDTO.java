181de14b3d62c40cf4a4feaf755b23211673b6a6
westorres9 <wester.torres9@gmail.com>
2022-09-05 17:41:34 -0300

    SaleDTO

---
+++ backend/src/main/java/com/devsuperior/ControleDeVendas/dto/SaleDTO.java
@@ -0,0 +1,72 @@
+package com.devsuperior.ControleDeVendas.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class SaleDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private LocalDate date;
+	private Integer deals;
+	private Integer visited;
+	private Double amount;
+	
+	public SaleDTO() {
+	}
+	
+	
+
+	public SaleDTO(Long id, LocalDate date, Integer deals, Integer visited, Double amount) {
+		this.id = id;
+		this.date = date;
+		this.deals = deals;
+		this.visited = visited;
+		this.amount = amount;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	
+
+}
