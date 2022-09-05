a48be99bd0c82cba9b997ea00978a9c2b183df8e
westorres9 <wester.torres9@gmail.com>
2022-09-05 17:37:31 -0300

    HashCode Equals byId

--- backend/src/main/java/com/devsuperior/ControleDeVendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/ControleDeVendas/entities/Sale.java
@@ -2,6 +2,7 @@ package com.devsuperior.ControleDeVendas.entities;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -72,6 +73,25 @@ public class Sale implements Serializable{
 	public void setAmount(Double amount) {
 		this.amount = amount;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sale other = (Sale) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 	
 	
 }
