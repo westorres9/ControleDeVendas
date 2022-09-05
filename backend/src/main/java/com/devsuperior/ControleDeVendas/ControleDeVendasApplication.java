978dc362261af2138ac5d32ca2c849a9baf82852
westorres9 <wester.torres9@gmail.com>
2022-09-05 17:14:40 -0300

    projeto inicial criado

---
+++ backend/src/main/java/com/devsuperior/ControleDeVendas/ControleDeVendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ControleDeVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeVendasApplication.class, args);
+	}
+
+}
