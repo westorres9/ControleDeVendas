a2f832f273b2b6661c3c5f1498233905e8a89de9
westorres9 <wester.torres9@gmail.com>
2022-09-06 16:13:01 -0300

    UserService UserInsert frontend

--- frontend/js/Services/UserService.js
+++ frontend/js/Services/UserService.js
@@ -1,7 +1,18 @@
-vendasApp.service('UserService', function($http) {
+vendasApp.service('UserService', function ($http) {
     var url = 'http://localhost:8080/users';
 
-    this.getUsers = function() {
+    this.getUsers = function () {
         return $http.get(url)
+    }
+
+    this.insertUser = function (user) {
+        var request = $http(
+            {
+                method: 'post',
+                url: url,
+                data: user
+            }
+        )
+        return request;
     }
 });
