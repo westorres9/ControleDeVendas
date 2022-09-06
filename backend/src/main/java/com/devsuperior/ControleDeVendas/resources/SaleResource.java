358bc072e00642c55fa0138c0728bc19f151614a
westorres9 <wester.torres9@gmail.com>
2022-09-06 07:51:32 -0300

    SaleResource Insert

--- backend/src/main/java/com/devsuperior/ControleDeVendas/resources/SaleResource.java
+++ backend/src/main/java/com/devsuperior/ControleDeVendas/resources/SaleResource.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +47,13 @@ public class SaleResource {
 		return ResponseEntity.created(uri).body(dto);
 		
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<SaleDTO> update(@PathVariable Long id, @RequestBody SaleDTO dto) {
+		SaleDTO newDTO = service.update(id, dto);
+		return ResponseEntity.ok().body(newDTO);
+	}
+	
 	
 
 }
