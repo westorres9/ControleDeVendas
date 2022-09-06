fdebb44a014d14badcdd5e554d24dbd2322e9ae1
westorres9 <wester.torres9@gmail.com>
2022-09-06 09:08:46 -0300

    UserService

---
+++ backend/src/main/java/com/devsuperior/ControleDeVendas/services/UserService.java
@@ -0,0 +1,91 @@
+package com.devsuperior.ControleDeVendas.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.ControleDeVendas.dto.RoleDTO;
+import com.devsuperior.ControleDeVendas.dto.UserDTO;
+import com.devsuperior.ControleDeVendas.entities.Role;
+import com.devsuperior.ControleDeVendas.entities.User;
+import com.devsuperior.ControleDeVendas.repositories.RoleRepository;
+import com.devsuperior.ControleDeVendas.repositories.UserRepository;
+import com.devsuperior.ControleDeVendas.services.exceptions.DatabaseException;
+import com.devsuperior.ControleDeVendas.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<UserDTO> findAllPaged(Pageable pageable) {
+		Page<User> page = repository.findAll(pageable);
+		return page.map(x -> new UserDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
+		return new UserDTO(entity);
+	}
+	
+	@Transactional
+	public UserDTO insert(UserDTO dto) {
+		User entity = new User();
+		copyDtoToEntity(entity, dto);
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+	
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = repository.getOne(id);
+			copyDtoToEntity(entity, dto);
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Entity not Found " + id);
+		}
+	}
+	
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Entity not Found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
+	public void copyDtoToEntity(User entity, UserDTO dto) {
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.getRoles().clear();
+		for(RoleDTO roleDTO : dto.getRoles()) {
+			Role role = roleRepository.getOne(roleDTO.getId());
+			entity.getRoles().add(role);
+		}
+	}
+	
+	
+	
+}
