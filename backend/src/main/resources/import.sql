f1d2bdee2303a3eac5d9d515c523a49ec8080177
westorres9 <wester.torres9@gmail.com>
2022-09-06 08:24:44 -0300

    seed banco Teams

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,7 @@
- INSERT INTO tb_sale (date,visited, deals, amount) VALUES ('2022-06-16',11,65,18196.0);
- INSERT INTO tb_sale (date,visited, deals, amount) VALUES ('2022-06-15',16,45,786.0);
- INSERT INTO tb_sale (date,visited, deals, amount) VALUES ('2022-06-14',18,32,2134.0);
+INSERT INTO tb_sale (date,visited, deals, amount) VALUES ('2022-06-16',11,65,18196.0);
+INSERT INTO tb_sale (date,visited, deals, amount) VALUES ('2022-06-15',16,45,786.0);
+INSERT INTO tb_sale (date,visited, deals, amount) VALUES ('2022-06-14',18,32,2134.0);
+  
+INSERT INTO tb_team (name) VALUES ('Bradesco');
+INSERT INTO tb_team (name) VALUES ('Santander');
+INSERT INTO tb_team (name) VALUES ('Picpay');
