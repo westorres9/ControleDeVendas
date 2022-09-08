f328f86d83b5c564c7e2d6c0810276b4fa2e38fa
westorres9 <wester.torres9@gmail.com>
2022-09-08 08:05:26 -0300

    UserServiceUpdate

--- frontend/js/Controllers/userController.js
+++ frontend/js/Controllers/userController.js
@@ -2,6 +2,15 @@ vendasApp.controller('UserController', function(UserService) {
     var vm = this;
     vm.title = 'UserController'
     vm.message = 'UserController'
+    vm.GetAllUsers = GetAllUsers;
+    vm.InsertUser = InsertUser;
+    vm.SelectUser = SelectUser;
+
+    function SelectUser (user) {
+        vm.user = user
+        console.log(vm.user);
+        return vm.user;
+    }
 
     function GetAllUsers () {
         var getAllUsers = UserService.getUsers();
@@ -15,4 +24,18 @@ vendasApp.controller('UserController', function(UserService) {
     }
 
     GetAllUsers();
+
+    function InsertUser() {
+        var user = vm.user;
+        
+        var insertUser = UserService.insert(user);
+        insertUser.then(function(response) {
+            vm.users = response.data.content;
+            console.log(response.data.content);
+        }).catch(function(error) {
+            console.log('ERROR: ' + error.status, error);
+        })
+    }
+
+    
 })
