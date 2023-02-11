ee27537620a40e298099af1427fe581a5ac37f23
westorres9 <wester.torres9@gmail.com>
2023-02-11 10:08:01 -0300

    sum-chart-bar-component

--- frontend/app/components/sum-chart-bar/sum-chart-bar.component.js
+++ frontend/app/components/sum-chart-bar/sum-chart-bar.component.js
@@ -5,16 +5,51 @@ function SumChartBarController(SaleService) {
     let date = [];
     let visited = [];
     let deals = [];
+
+
+
+    let chart = Highcharts.chart('sum-chart-bar', {
+        title: {
+            text: 'Vendas por periodo'
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            categories:[]
+        },
+        yAxis: {
+            title: {
+                text: ''
+            }
+        }
+    });
+
     function getAllSales() {
-        SaleService.getAllSales($ctrl.page, $ctrl.size, $ctrl.mindate, $ctrl.maxdate).then((response) => {
+        SaleService.getAllSales($ctrl.mindate, $ctrl.maxdate).then((response) => {
             sales = response.data.content;
             sales.forEach(item => date.push(item.date));
             console.log(sales);
             console.log(date);
+
+            chart.xAxis[0].setCategories(date)
+
+            chart.addSeries({
+                name: 'Visitas',
+                data: [visited]
+            });
+            chart.addSeries({
+                name: 'Vendas',
+                data: [deals]
+            })
         })
     }
 
     $ctrl.$onInit = function() {
+        
+    }
+
+    $ctrl.$onChanges = function(mindate, maxdate) {
         getAllSales();
     }
    
